101124c6166797b6539929cf23f3dac30554e00b
cyberowl <cybrowl@protonmail.com>
2022-12-26 04:20:10 -0800

    add: data grid

---
+++ src/lib/components/DataGrid.svelte
@@ -0,0 +1,48 @@
+<script>
+    export let data = [];
+    export let columns = [];
+</script>
+
+<table>
+    <tr>
+        {#each columns as column}
+            <th on:resize={e => (e.target.style.width = `${e.target.offsetWidth}px`)}>
+                {column}
+            </th>
+        {/each}
+    </tr>
+    {#each data as row}
+        <tr>
+            {#each columns as column}
+                <td>{row[column]}</td>
+            {/each}
+        </tr>
+    {/each}
+</table>
+
+<style>
+    table {
+        border-collapse: collapse;
+        width: 100%;
+    }
+
+    td,
+    th {
+        border: 1px solid #444;
+        color: aquamarine;
+        padding: 8px;
+    }
+
+    tr:nth-child(even) {
+        background-color: #333;
+    }
+
+    tr:nth-child(odd) {
+        background-color: #222;
+    }
+
+    th {
+        background-color: #555;
+        color: white;
+    }
+</style>
---
+++ src/stories/DataGrid.stories.svelte
@@ -0,0 +1,34 @@
+<script>
+    import {Meta, Template, Story} from '@storybook/addon-svelte-csf';
+    import DataGrid from '../lib/components/DataGrid.svelte';
+</script>
+
+<Meta
+    title="Heartbeat/DataGrid"
+    component={DataGrid}
+    argTypes={{
+        onClick: {action: 'onClick'},
+        onClose: {action: 'onClose'}
+    }}
+/>
+
+<Template let:args>
+    <DataGrid {...args} on:click={args.onClick} />
+</Template>
+
+<Story
+    name="Default"
+    args={{
+        data: [
+            {
+                cycles_balance: 3100000000000,
+                memory_in_mb: 0,
+                heap_in_mb: 0,
+                name: 'Explore',
+                time: 1672054626199920000,
+                id: 'qvhpv-4qaaa-aaaaa-aaagq-cai'
+            }
+        ],
+        columns: ['id', 'time', 'name', 'cycles_balance', 'memory_in_mb', 'heap_in_mb']
+    }}
+/>
